3783d136f96b10285f7a698673e1519b7ff95c90
broskoy <mihnea.buzoiu@gmail.com>
2025-10-04 23:30:43 +0200

    Added camera transition (only on right tower for now)

--- Scenes/Scripts/game.gd
+++ Scenes/Scripts/game.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
-func _process(delta: float) -> void:
-	var camera = $Camera2D
-	var p2 = $Player2
-	camera.position.x = p2.position.x   # follow only X
-	# leave Y unchanged
+
+func _on_player_entered_tower(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		_transition_camera_to(Vector2(793, $Camera2D.position.y))  # new camera target position
+
+
+func _transition_camera_to(target: Vector2) -> void:
+	var tween = create_tween()
+	tween.tween_property($Camera2D, "position", target, 1.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
