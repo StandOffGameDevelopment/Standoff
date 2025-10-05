a4b1725aaa11a46146a7e49e49e8ac075b9c47af
IsikAli01 <isikaliakpinar1@gmail.com>
2025-10-05 16:04:12 +0200

    Camera Transition Improved
    Guys I made the camera transition dependent on the x coordinate of the current camera angle and the player passing through the door so now the logic for camera transition is somewhat okay. But in order it to make sense we need to define a boolean for a player being dead and depend the gate opening scene to that so each player can not go through the gate whenever and confuse the game.

--- Scenes/Scripts/game.gd
+++ Scenes/Scripts/game.gd
@@ -2,8 +2,17 @@ extends Node2D
 
 
 func _on_player_entered_tower(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		_transition_camera_to(Vector2(793, $Camera2D.position.y))  # new camera target position
+	if body is Player_1:
+		if $Camera2D.position.x == -793:
+			_transition_camera_to(Vector2(0, $Camera2D.position.y))  # new camera target position
+		if $Camera2D.position.x == 0:
+			_transition_camera_to(Vector2(793, $Camera2D.position.y))  # new camera target position
+		
+	if body is Player_2:
+		if $Camera2D.position.x == 793:
+			_transition_camera_to(Vector2(0, $Camera2D.position.y))  # new camera target position
+		if $Camera2D.position.x == 0:
+			_transition_camera_to(Vector2(-793, $Camera2D.position.y))  # new camera target position
 
 
 func _transition_camera_to(target: Vector2) -> void:
